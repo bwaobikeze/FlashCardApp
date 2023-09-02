13c19d492bd22f508243b28884da86961c6c85ce
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-09-02 01:41:07 -0500

    added images

--- Exercise2_Waobikeze_Brian/ContentView.swift
+++ Exercise2_Waobikeze_Brian/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cardViewImages: [UIImage] = [UIImage(imageLiteralResourceName: "1"), UIImage(imageLiteralResourceName: "2"), UIImage(imageLiteralResourceName: "3")]
     var body: some View {
         VStack {
+            Text("CardHub").font(.title)
+            Spacer()
+                
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
         }
         .padding()
     }
