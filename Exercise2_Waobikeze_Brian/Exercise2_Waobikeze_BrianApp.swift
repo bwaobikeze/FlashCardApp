3f2b9a4fe30241b890670a47e270f9a0f58077cc
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-09-02 00:17:11 -0500

    Initial Commit

---
+++ Exercise2_Waobikeze_Brian/Exercise2_Waobikeze_BrianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise2_Waobikeze_BrianApp.swift
+//  Exercise2_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise2_Waobikeze_BrianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
