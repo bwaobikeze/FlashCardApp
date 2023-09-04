cbb3b0c1542144c723e0a37a5b5a602c9d4f45c8
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-09-03 23:28:22 -0500

    implemented function buttons of the first two and added the list of descriptions

--- Exercise2_Waobikeze_Brian/ContentView.swift
+++ Exercise2_Waobikeze_Brian/ContentView.swift
@@ -9,19 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
      @State var cardViewIdx: Int = 0
+    @State var cardTitleidx: Int = 0
+    @State var cardDecriptionIdx: Int = 0
     let cardViewImages: [UIImage] = [UIImage(imageLiteralResourceName: "1"), UIImage(imageLiteralResourceName: "2"), UIImage(imageLiteralResourceName: "3")]
+    let CardTitle: [String] = ["View Controller","UIKit","UIAlertController"]
+    let CardDecription: [List] = [    List {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+        Text("defines the behavior for common VCs")
+        Text("updates the content of the view")
+        Text("responding to user interactions")
+        Text("resizing views and layout mgmnt")
+        Text("coordinating with other objects")
+    },    List {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+        Text("provides required IOS infrastructure")
+        Text("window and view architecture")
+        Text("event handling for multi-touch and etc")
+        Text("manages interaction with system")
+        Text("a lot of features incl. resource mgmnt")
+    },    List {
+        Text("configure alerts and action sheets")
+        Text("intended to be used as-is")
+        Text("does not support subclassing")
+        Text("inherits from UIViewController")
+        Text("support text fields to the alert interface")
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+    }]
+
     var body: some View {
         VStack {
-            Text("CardHub").font(.title)
+            Text("CardHub").font(.title.bold())
             Image(uiImage: cardViewImages[cardViewIdx]).resizable().frame(width: 90, height: 90)
             
             ZStack(alignment: .top){
                 Rectangle().fill(.gray).frame(width: 330, height: 270)
                 
-                Rectangle().frame(width: 330, height: 50)
-                    .foregroundColor(.cyan)
+                Rectangle().frame(width: 330, height: 55)
+                    .foregroundColor(.cyan).overlay(Text("Card: "+CardTitle[cardTitleidx])
+                        .font(.title.bold()).multilineTextAlignment(.leading)
+                    )
             }
-            
             Spacer()
             Button(action: randomCard) {
                 Text("Random Card").frame(width: 300, height: 40)
@@ -41,10 +68,13 @@ struct ContentView: View {
         .padding()
     }
     func nextCard (){
+        cardTitleidx=(cardTitleidx+1)%3
+        cardViewIdx=cardTitleidx
         
     }
     func randomCard(){
-        
+        cardTitleidx = Int.random(in: 0..<3)
+        cardViewIdx = cardTitleidx
     }
     func CardSelect(){
         
